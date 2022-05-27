c2097ffe841712af6f22d79bea3f49f6f6ff6825
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-05-27 12:25:17 +0530

    Login Update 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import Login from "./components/Login";
+import { auth } from "./firebase";
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch=useDispatch();
+  // const displatch
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    //eslint-disable-next-line
+  }, []);
   return (
     <div className="App">
-      {/* Header */}
-      <Header/>
-      {/* App Body */}
-      <div className="app_body">
-        <Sidebar/>
-        {/* Sidebar */}
-        {/* Feed */}
-        {/* Widgets */}
-      </div>
+      <Header />
+      { !user ? (
+        <Login/>
+      ) : (
+        <div className="app_body">
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
       
-       {/* Sidebar */}
-       {/* Feed */}
-       {/* Widgets */}
+      {/* Widgets */}
     </div>
   );
 }
